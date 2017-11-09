a1418401340edfde60da0288fdd1febaf2ffc1f7
zhe-he <luanhong_feiguo@sina.com>
2017-11-09 21:41:44 +0800

    2

--- read.md
+++ read.md
@@ -1,6 +1,13 @@
 ### 接口地址    
 微信openId 在index.html 第13行 ， window.openId = 'wxId123456'   
-上传接口url 在 treasureHunt.js 124行，  `var url = "/send?id=" + window.openId`    
+上传接口url 在 treasureHunt.js 152行，  
+var json = {
+    "id": window.openId,  
+    "prize": this.rPrize.code,   // 0 -> 没中奖,1 -> Q版公仔,2 -> 文具包,3 -> 电影票
+    "msg": this.rPrize.msg // 中奖的文案说明
+}
+`var url = "/send"`   GET 请求 id 、 prize 、 msg    
+$_GET['id'] ...  
 
 ### 图片替换    
 `我的奖品`图片在 images/static/prize.png 
--- src/treasureHunt.js
+++ src/treasureHunt.js
@@ -1,5 +1,6 @@
 import "./styles/index.scss";
 import Vue from "vue";
+import querystring from "querystring";
 var level = require('./data/level');
 
 new Vue({
@@ -16,7 +17,10 @@ new Vue({
         msg: '',
         showMsg: false,
         msgTimer: null,
-        level: 1
+        level: 0,
+        tips: ["这一关的线索是跟“记录”有关哦","这一关的线索是“绿色”","这一关的提示是凉快","这一关的提示是“美妙”"],
+        rPrize: {},
+        isEnd: false
     },
     mounted(){
         var isMobile = 'touchstart' in window;
@@ -30,26 +34,49 @@ new Vue({
                 }
             });
         }
+        this.rPrize = this.rndPrize();
     },
     methods: {
+        rndPrize(){
+            var t = Math.random();
+            var msgL = "恭喜您，找齐碎片。获得";
+            var msgR = "礼品。请截图本页按规则领取奖品哦。";
+            if (t <= 50/2500) {
+                return {msg: msgL + '电影票' + msgR, code: 3};
+            }else if(t <= 150/2500){
+                return {msg: msgL + '文具包' + msgR, code: 2};
+            }else if(t <= 650/2500){
+                return {msg: msgL + '华府定制Q版公仔' + msgR, code: 1};
+            }else{
+                return {msg:"抱歉您未获得礼品，可以转发朋友圈再试试哦。",code:0};
+            }
+        },
         showToast(card){
             // if (this.showMsg) {
             //     return ;
             // }
-            if (card.prize == 4 && this.prize[3].has) {
+            if (card.prize == 4 && !this.isEnd) {
+                this.isEnd = true;
                 // ajax
-                var url = "/send?id=" + window.openId;
+                var json = {
+                    "id": window.openId,
+                    "prize": this.rPrize.code,
+                    "msg": this.rPrize.msg
+                }
+                var url = "/send?" + querystring.stringify(json);
                 fetch(url); 
             }
             clearTimeout(this.msgTimer);
             this.showMsg = true;
-            if (this.prize[3].has) {
-                this.msg = '您已集齐所有的碎片';
-            }else{
-                this.msg = card.text;
-            }
+
+            
             if (card.prize > 0) {
                 this.prize[card.prize-1].has = true;
+            }
+            if (card.prize == 4 && this.isEnd) {
+                this.msg = this.rPrize.msg;
+            }else{
+                this.msg = card.text;
             }
             
             this.msgTimer = setTimeout(()=>{
@@ -59,6 +86,9 @@ new Vue({
                 }
                 if (card.prize == 4) {
                     this.showPrize = true;
+                    if (this.isEnd) {
+                        this.showMsg = true;
+                    }
                 }
 
             },1500);
